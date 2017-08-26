9304214228056e902076e01c59739d79487d7a0f
govendha <30722966+govendha@users.noreply.github.com>
2017-08-26 16:45:39 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char i;
+    printf("Enter a character: ");
+    scanf("%c",&i);
+
+    if( (i>='a' && i<='z') || (i>='A' && i<='Z'))
+        printf("%c is an alphabet.",i);
+    else
+        printf("%c is not an alphabet.",i);
+
+    return 0;
+}
